2657abcf976a8c81a15067bb1e1d74c962822ff4
GreenSkinFrog <admin@crystal1113.com>
2018-03-08 16:58:16 +0800

    120987661

--- c_code/0003_data_integer/data_integer.c
+++ c_code/0003_data_integer/data_integer.c
@@ -2,8 +2,18 @@
 
 int main()
 {
-    int 
-    printf("hello world");
-    getchar();
-    return 0;
+	char c;
+	short s=100;
+	int a,b;
+	int sum = 0;
+	
+	a=20;
+	b=5;
+	sum=a+b;
+	printf("c is %d,s is %d\n",c,s);
+	printf("a is %d,b is %d\n",a,b);
+	printf("sum=%d,a-b=%d",sum,a-b);
+	
+	return 0;
 }
+
